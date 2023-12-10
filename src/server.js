f65200133ce763a5ed653d8763b1629360ab1830
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-12-10 12:03:15 +1100

    cors amend origin

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const cors = require('cors');
 const corsOptions = {
-	origin: ["http://localhost:3000", "https://blah"],
+	origin: "http://localhost:3000",
 	optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
